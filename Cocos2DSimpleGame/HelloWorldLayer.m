385800d49b8af31fc9cabb29ff233d9cf9921dc7
Zeeshan Khaliq <shan@Zeeshans-Mac.local>
2013-08-20 18:50:27 -0700

    Added Monsters

--- Cocos2DSimpleGame/HelloWorldLayer.m
+++ Cocos2DSimpleGame/HelloWorldLayer.m
@@ -34,75 +34,56 @@
 	return scene;
 }
 
+- (void)addMonster
+{
+    CCSprite *monster = [CCSprite spriteWithFile:@"monster.png"];
+    
+    // Determine where to spawn the monster along the Y axis
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    int minY = monster.contentSize.height/2;
+    int maxY = winSize.height - monster.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    // Create the monster slightly off-screen along the right edge
+    // and along a random position along the Y axis as calculated above
+    monster.position = ccp(winSize.width + monster.contentSize.width/2, actualY);
+    [self addChild:monster];
+    
+    // Determine speed of the monster
+    int minDuration = 2;
+    int maxDuration = 4;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    // Create the actions
+    CCMoveTo *actionMove = [CCMoveTo actionWithDuration:actualDuration
+                                               position:ccp(-monster.contentSize.width/2, actualY)];
+    CCCallBlockN *actionMoveDone = [CCCallBlockN actionWithBlock:^(CCNode *node)
+                                    { [node removeFromParentAndCleanup:YES] ;}];
+    
+    [monster runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+    
+}
+
+- (void)gameLogic:(ccTime)dt
+{
+    [self addMonster];
+}
+
 // on "init" you need to initialize your instance
 -(id) init
 {
-	// always call "super" init
-	// Apple recommends to re-assign "self" with the "super's" return value
-	if( (self=[super init]) ) {
-		
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
-
-		// ask director for the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
-		
-		
-		
-		//
-		// Leaderboards and Achievements
-		//
-		
-		// Default font size will be 28 points.
-		[CCMenuItemFont setFontSize:28];
-		
-		// to avoid a retain-cycle with the menuitem and blocks
-		__block id copy_self = self;
-		
-		// Achievement Menu Item using blocks
-		CCMenuItem *itemAchievement = [CCMenuItemFont itemWithString:@"Achievements" block:^(id sender) {
-			
-			
-			GKAchievementViewController *achivementViewController = [[GKAchievementViewController alloc] init];
-			achivementViewController.achievementDelegate = copy_self;
-			
-			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-			
-			[[app navController] presentModalViewController:achivementViewController animated:YES];
-			
-			[achivementViewController release];
-		}];
-		
-		// Leaderboard Menu Item using blocks
-		CCMenuItem *itemLeaderboard = [CCMenuItemFont itemWithString:@"Leaderboard" block:^(id sender) {
-			
-			
-			GKLeaderboardViewController *leaderboardViewController = [[GKLeaderboardViewController alloc] init];
-			leaderboardViewController.leaderboardDelegate = copy_self;
-			
-			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-			
-			[[app navController] presentModalViewController:leaderboardViewController animated:YES];
-			
-			[leaderboardViewController release];
-		}];
-
-		
-		CCMenu *menu = [CCMenu menuWithItems:itemAchievement, itemLeaderboard, nil];
-		
-		[menu alignItemsHorizontallyWithPadding:20];
-		[menu setPosition:ccp( size.width/2, size.height/2 - 50)];
-		
-		// Add the menu to the layer
-		[self addChild:menu];
-
-	}
+    if ((self = [super initWithColor:ccc4(255, 255, 255, 255)]))
+    {
+        CGSize winSize = [CCDirector sharedDirector].winSize;
+        CCSprite *player = [CCSprite spriteWithFile:@"player.png"];
+        player.position = ccp(player.contentSize.width/2, winSize.height/2);
+        [self addChild:player];
+        [self schedule:@selector(gameLogic:) interval:1.0];
+        [self setTouchEnabled:YES];
+    }
+    
 	return self;
 }
 
