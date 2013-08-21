ff62159f88102c6fb31adce96f3b80324e491d0a
Zeeshan Khaliq <shan@Zeeshans-Mac.local>
2013-08-20 17:46:54 -0700

    Initial Commit

---
+++ Cocos2DSimpleGame/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Cocos2DSimpleGame
+//
+//  Created by Zeeshan Khaliq on 8/20/13.
+//  Copyright Zeeshan Khaliq 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
